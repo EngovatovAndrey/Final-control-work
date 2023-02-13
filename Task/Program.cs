2dcb6e1a5353d74495ae5cd598c3c117e0fdea2c
Andrey Engo <5522662@gmail.com>
2023-02-13 14:20:09 +0300

    add main programm

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует массив строк, 
+длинна которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется использовать коллекции, 
+лучше обойтись исключительно массивами. 
+
+Примеры:
+['hello', '2', 'world', ':-)'] -> ['2', ':-)']
+['1234', '1567', '-2', 'computer science'] -> [ '-2' ]
+['Russia', 'Denmark', 'Kazan'] -> [] 
+*/
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите произвольное количество строк через пробел");
+string[] userArray = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+WriteLine("[{0}]", string.Join(", ", SmallArray(userArray)));
+
+string[] SmallArray(string[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        { 
+            count +=1;
+        }
+    } 
+    string[] new_array = new string[count];
+    int index = 0;
+    for(int j = 0; j < array.Length; j++)
+    {
+        
+        if (array[j].Length <= 3)
+        { 
+            new_array[index] = array[j];
+            index +=1;
+        }
+    }
+    return new_array;
+}
